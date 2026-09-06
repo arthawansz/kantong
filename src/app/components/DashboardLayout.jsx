64f2be700231ce6771c5055a391bde3f75d08cd6
Arthawan Pratama Pakurimba Azzuhud <arthaganteng111@gmail.com>
2026-09-06 14:17:25 +0800

    add shared dashboard layout

---
+++ src/app/components/DashboardLayout.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import AppHeader from "./AppHeader";
+import AppSidebar from "./AppSidebar";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="min-h-screen bg-[#f6f7f9] text-[#111827]">
+      <div className="mx-auto flex min-h-screen max-w-[1680px]">
+        <AppSidebar />
+        <main className="min-w-0 flex-1">
+          <AppHeader />
+          <div className="px-5 py-6 md:px-8 lg:px-10 lg:py-8">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
+}
