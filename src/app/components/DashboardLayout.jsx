9af8d1e0b50dca38910b6867a0ed997c5b4f3d57
Arthawan Pratama Pakurimba Azzuhud <arthaganteng111@gmail.com>
2026-09-07 10:56:07 +0800

    sync layout offsets with collapsible sidebar header

--- src/app/components/DashboardLayout.jsx
+++ src/app/components/DashboardLayout.jsx
@@ -15,19 +15,24 @@ export default function DashboardLayout({ children }) {
     return children;
   }
 
+  const handleToggleSidebar = () => {
+    setSidebarCollapsed((value) => !value);
+  };
+
   return (
     <div className="min-h-screen bg-[#f6f7f9] text-[#111827]">
-      <AppSidebar
+      <AppSidebar collapsed={sidebarCollapsed} />
+
+      <AppHeader
         collapsed={sidebarCollapsed}
-        onToggle={() => setSidebarCollapsed((value) => !value)}
+        onToggle={handleToggleSidebar}
       />
 
       <main
-        className={`min-w-0 transition-[margin] duration-300 ${
+        className={`min-w-0 pt-[76px] transition-[margin] duration-300 ${
           sidebarCollapsed ? "lg:ml-[84px]" : "lg:ml-[250px]"
         }`}
       >
-        <AppHeader />
         <div className="px-5 py-6 md:px-8 lg:px-10 lg:py-8">{children}</div>
       </main>
     </div>
