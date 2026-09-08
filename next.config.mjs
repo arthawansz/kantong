f2fc9989f671e794de19d0aed32871935562a717
cuyngoding <arthwanpratama1@gmail.com>
2026-09-09 01:44:52 +0800

    feat: need fix IP addres bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devIndicators: false,
-    allowedDevOrigins: ["10.110.2.190"],
-    turbopack: {
-        root: process.cwd(),
-    },
+  devIndicators: false,
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
